0c5479bee0212bdfa5660c02dca1090dc2f08e8d
BakaminovD <101690730+BakaminovD@users.noreply.github.com>
2024-12-27 15:58:13 +0300

    Update script.js
    First commit/ New site

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let burger = document.querySelector('.burger'),
+    touch = document.querySelector('.burger-touch'),
+    main = document.querySelector('main'),
+    footer = document.querySelector('footer'),
+    menu = document.querySelector('.mobail-menu'),
+    pages = document.querySelector('.pages');
 
+burger.addEventListener('click', () => {
+    touch.style.display = 'flex';
+    menu.style.display = 'flex';
+    burger.style.display = 'none';
+    main.style.display = 'none';
+    footer.style.display = 'none';
+});
+
+touch.addEventListener('click', () => {
+    burger.style.display = 'flex';
+    main.style.display = 'flex';
+    footer.style.display = 'flex';
+    touch.style.display = 'none';
+    menu.style.display = 'none';
+});
+
+pages.addEventListener('click', () => {
+    burger.style.display = 'flex';
+    main.style.display = 'flex';
+    footer.style.display = 'flex';
+    touch.style.display = 'none';
+    menu.style.display = 'none';
+})
